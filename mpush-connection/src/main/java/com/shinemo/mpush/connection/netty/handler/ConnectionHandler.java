94c70fbf22e42a5dfcecfa61c8d2435e2fde93b8
黄志磊 <huangzl@shinemo.com>
2015-12-24 09:42:27 +0800

    add log

--- mpush-connection/src/main/java/com/shinemo/mpush/connection/netty/handler/ConnectionHandler.java
+++ mpush-connection/src/main/java/com/shinemo/mpush/connection/netty/handler/ConnectionHandler.java
@@ -14,40 +14,62 @@ import io.netty.channel.ChannelPromise;
 
 import java.net.SocketAddress;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by ohun on 2015/12/19.
  */
 public class ConnectionHandler extends ChannelHandlerAdapter {
+	
+	private static final Logger log = LoggerFactory.getLogger(ConnectionHandler.class);
+	
     private MessageReceiver receiver = new MessageReceiver();
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    	log.warn("",ctx.channel().remoteAddress()+",  channelRead");
     	Connection connection = ConnectionManager.INSTANCE.get(ctx.channel());
         receiver.onMessage(new Request((Packet) msg, connection));
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        super.exceptionCaught(ctx, cause);
         ConnectionManager.INSTANCE.remove(ctx.channel());
+        log.warn("",ctx.channel().remoteAddress()+", exceptionCaught");
     }
 
     @Override
     public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) throws Exception {
-        super.connect(ctx, remoteAddress, localAddress, promise);
+    	log.warn("",ctx.channel().remoteAddress()+",  connect");
+    	super.connect(ctx, remoteAddress, localAddress, promise);
         Connection connection = new NettyConnection();
         connection.init(ctx.channel());
         ConnectionManager.INSTANCE.add(connection);
     }
+    
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    	log.warn("",ctx.channel().remoteAddress()+",  channelActive");
+    	super.channelActive(ctx);
+    }
+    
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+    	log.warn("",ctx.channel().remoteAddress()+",  channelInactive");
+    	super.channelInactive(ctx);
+    }
 
     @Override
     public void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+    	log.warn("",ctx.channel().remoteAddress()+",  disconnect");
         super.disconnect(ctx, promise);
         ConnectionManager.INSTANCE.remove(ctx.channel());
     }
 
     @Override
     public void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
+    	log.warn("",ctx.channel().remoteAddress()+", close");
         super.close(ctx, promise);
         ConnectionManager.INSTANCE.remove(ctx.channel());
     }
