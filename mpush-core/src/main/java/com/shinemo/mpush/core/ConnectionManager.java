2df06cffe2a9ac271c6567cb129339c1afa5204a
黄志磊 <huangzl@shinemo.com>
2015-12-24 09:42:27 +0800

    add log

--- mpush-core/src/main/java/com/shinemo/mpush/core/ConnectionManager.java
+++ mpush-core/src/main/java/com/shinemo/mpush/core/ConnectionManager.java
@@ -6,6 +6,8 @@ import com.shinemo.mpush.api.Connection;
 import io.netty.channel.Channel;
 import io.netty.util.internal.chmv8.ConcurrentHashMapV8;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
 
@@ -18,34 +20,8 @@ public class ConnectionManager {
     //可能会有20w的链接数
     private final ConcurrentMap<String, Connection> connections = new ConcurrentHashMapV8<String, Connection>();
 
-//	private final Cache<String,NettyConnection> cacherClients = CacheBuilder.newBuilder()
-//		      .maximumSize(2<<17)
-//		      .expireAfterAccess(27, TimeUnit.MINUTES)
-//		      .removalListener(new RemovalListener<String, NettyConnection>() {
-//		    	  public void onRemoval(RemovalNotification<String,NettyConnection> notification) {
-//		    		  if(notification.getValue().isClosed()){
-////		    			  notification.getValue().close("[Remoting] removed from cache");
-//		    		  }
-//		    	  };
-//			}).build();
-    
     public Connection get(final String channelId) throws ExecutionException {
-    	
-//    	NettyConnection client = cacherClients.get(channelId, new Callable<NettyConnection>() {
-//            @Override
-//            public NettyConnection call() throws Exception {
-//            	NettyConnection client = getFromRedis(channelId);
-//                return client;
-//            }
-//        });
-//        if (client == null || !client.isClosed()) {
-//            cacherClients.invalidate(channelId);
-//            return null;
-//        }
-//        return client;
-    	
     	return connections.get(channelId);
-    	
     }
     
     public Connection get(final Channel channel){
@@ -68,6 +44,14 @@ public class ConnectionManager {
     
     public void remove(Channel channel){
     	connections.remove(channel.id().asLongText());
+    }
+    
+    public List<String> getConnectionIds(){
+    	return new ArrayList<String>(connections.keySet());
+    }
+    
+    public List<Connection> getConnections(){
+    	return new ArrayList<Connection>(connections.values());
     }
     
 }
