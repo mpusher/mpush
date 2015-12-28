c6aebd8dd75beb526fb757167fe568da894b1d53
夜色 <ohun@live.cn>
2015-12-28 03:47:53 +0000

    代码整理

--- mpush-tools/src/main/java/com/shinemo/mpush/tools/thread/ThreadPoolUtil.java
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/thread/ThreadPoolUtil.java
@@ -8,23 +8,25 @@ import com.shinemo.mpush.tools.Constants;
 
 public class ThreadPoolUtil {
 
-	private static final ThreadPoolManager threadPoolManager = new ThreadPoolManager(Constants.MIN_POOL_SIZE, Constants.MAX_POOL_SIZE,
-			Constants.THREAD_QUEUE_SIZE);
-	
-	private static Executor bossExecutor = ThreadPoolUtil.getThreadPoolManager().getThreadExecutor(ThreadNameSpace.NETTY_BOSS,Constants.MIN_BOSS_POOL_SIZE,Constants.MAX_BOSS_POLL_SIZE);
-    private static Executor workExecutor = ThreadPoolUtil.getThreadPoolManager().getThreadExecutor(ThreadNameSpace.NETTY_WORKER,Constants.MIN_WORK_POOL_SIZE,Constants.MAX_WORK_POOL_SIZE);
+    private static final ThreadPoolManager threadPoolManager =
+            new ThreadPoolManager(Constants.MIN_POOL_SIZE, Constants.MAX_POOL_SIZE, Constants.THREAD_QUEUE_SIZE);
 
-	public static ThreadPoolManager getThreadPoolManager() {
-		return threadPoolManager;
-	}
-	
-	public static Executor getBossExecutor(){
-		return bossExecutor;
-	}
-	
-	public static Executor getWorkExecutor(){
-		return workExecutor;
-	}
-	
-	
+    private static Executor bossExecutor = ThreadPoolUtil.getThreadPoolManager()
+            .getThreadExecutor(ThreadNameSpace.NETTY_BOSS, Constants.MIN_BOSS_POOL_SIZE, Constants.MAX_BOSS_POLL_SIZE);
+    private static Executor workExecutor = ThreadPoolUtil.getThreadPoolManager()
+            .getThreadExecutor(ThreadNameSpace.NETTY_WORKER, Constants.MIN_WORK_POOL_SIZE, Constants.MAX_WORK_POOL_SIZE);
+
+    public static ThreadPoolManager getThreadPoolManager() {
+        return threadPoolManager;
+    }
+
+    public static Executor getBossExecutor() {
+        return bossExecutor;
+    }
+
+    public static Executor getWorkExecutor() {
+        return workExecutor;
+    }
+
+
 }
