5626936d46acb1c3324f2ce044699c6da8ef93ac
闫逍旭 <yanxx@shinemo.com>
2015-12-28 15:46:20 +0000

    消息重构

--- mpush-api/src/main/java/com/shinemo/mpush/api/RouterInfo.java
+++ mpush-api/src/main/java/com/shinemo/mpush/api/RouterInfo.java
@@ -3,7 +3,7 @@ package com.shinemo.mpush.api;
 /**
  * Created by ohun on 2015/12/23.
  */
-public class RouterInfo {
+public final class RouterInfo {
     private String serverIp;
     private String osName;
     private String clientVer;
