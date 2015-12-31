fa1b7c3a24371be36eb79bb7d590303d7c8318be
黄志磊 <huangzl@shinemo.com>
2015-12-31 13:57:22 +0800

    同步修改app内存中的值

--- mpush-tools/src/main/java/com/shinemo/mpush/tools/zk/listener/impl/ConnectionPathListener.java
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/zk/listener/impl/ConnectionPathListener.java
@@ -1,16 +1,20 @@
 package com.shinemo.mpush.tools.zk.listener.impl;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
 import org.apache.curator.framework.recipes.cache.TreeCacheEvent.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Maps;
 import com.shinemo.mpush.tools.Jsons;
 import com.shinemo.mpush.tools.zk.PathEnum;
 import com.shinemo.mpush.tools.zk.ServerApp;
@@ -26,7 +30,7 @@ public class ConnectionPathListener implements CallBack{
 	
 	private static final Logger log = LoggerFactory.getLogger(ConnectionPathListener.class);
 	
-	private List<ServerApp> appList = new ArrayList<ServerApp>();
+	private static Map<String,ServerApp> holder = Maps.newConcurrentMap();
 	
 	@Override
 	public void handler(CuratorFramework client, TreeCacheEvent event, String path) {
@@ -35,10 +39,13 @@ public class ConnectionPathListener implements CallBack{
 			data = ToStringBuilder.reflectionToString(event.getData(), ToStringStyle.MULTI_LINE_STYLE);
 		}
 		if (Type.NODE_ADDED == event.getType()) {
+			dataAddOrUpdate(event.getData());
 			log.warn("ConnectionPathListener path:" + path + ", node Add"+","+data);
 		} else if (Type.NODE_REMOVED == event.getType()) {
+			dataRemove(event.getData());
 			log.warn("ConnectionPathListener path:" + path + ", node Remove"+","+data);
 		} else if (Type.NODE_UPDATED == event.getType()) {
+			dataAddOrUpdate(event.getData());
 			log.warn("ConnectionPathListener path:" + path + "," + "node update"+","+data);
 		} else {
 			log.warn("ConnectionPathListener other path:" + path + "," + event.getType().name()+","+data);
@@ -56,21 +63,37 @@ public class ConnectionPathListener implements CallBack{
 	private void getData(){
 		//获取机器列表
 		List<String> rawData = ZkUtil.instance.getChildrenKeys(PathEnum.CONNECTION_SERVER_ALL_HOST.getPath());
-		List<ServerApp> newAppList = new ArrayList<ServerApp>();
 		for(String raw:rawData){
-			String rawApp = ZkUtil.instance.get(PathEnum.CONNECTION_SERVER_ALL_HOST.getPathByName(raw));
-			ServerApp app = Jsons.fromJson(rawApp, ServerApp.class);
-			newAppList.add(app);
+			String fullPath = PathEnum.CONNECTION_SERVER_ALL_HOST.getPathByName(raw);
+			ServerApp app = getServerApp(raw);
+			holder.put(fullPath, app);
 		}
-		appList = newAppList;
 	}
 	
-	public List<ServerApp> getAppList(){
-		return appList;
+	private void dataRemove(ChildData data){
+		String path = data.getPath();
+		holder.remove(path);
+	}
+	
+	private void dataAddOrUpdate(ChildData data){
+		String path = data.getPath();
+		byte[] rawData = data.getData();
+		ServerApp serverApp = Jsons.fromJson(rawData, ServerApp.class);
+		holder.put(path, serverApp);
+	}
+	
+	private ServerApp getServerApp(String fullPath){
+		String rawApp = ZkUtil.instance.get(fullPath);
+		ServerApp app = Jsons.fromJson(rawApp, ServerApp.class);
+		return app;
+	}
+	
+	public Collection<ServerApp> getAppList(){
+		return Collections.unmodifiableCollection(holder.values());
 	}
 
 	private void printAppList(){
-		for(ServerApp app:appList){
+		for(ServerApp app:holder.values()){
 			log.warn(ToStringBuilder.reflectionToString(app, ToStringStyle.DEFAULT_STYLE));
 		}
 	}
