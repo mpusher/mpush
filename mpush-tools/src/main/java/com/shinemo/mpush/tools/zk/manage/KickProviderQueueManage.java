d72a9bc106a14f0ed651f1d65deec1f3556fb8cf
黄志磊 <huangzl@shinemo.com>
2015-12-31 18:03:47 +0800

    add kick provider queue manage

---
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/zk/manage/KickProviderQueueManage.java
@@ -0,0 +1,13 @@
+package com.shinemo.mpush.tools.zk.manage;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.shinemo.mpush.tools.zk.ServerApp;
+import com.shinemo.mpush.tools.zk.queue.Provider;
+
+public class KickProviderQueueManage {
+
+	private static List<Provider<ServerApp>> providers = Lists.newArrayList();
+	
+}
