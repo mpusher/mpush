d715e5242c8ade0679b974b38cf0722ba6af6d87
闫逍旭 <yanxx@shinemo.com>
2016-01-07 02:32:26 +0000

    模块依赖整理

--- mpush-tools/src/main/java/com/shinemo/mpush/tools/Jsons.java
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/Jsons.java
@@ -16,88 +16,94 @@ import java.util.Map;
  * Created by xiaoxu.yxx on 15/8/7.
  */
 public final class Jsons {
-	private static final Logger LOGGER = LoggerFactory.getLogger(Jsons.class);
-	public static final Gson GSON = new GsonBuilder().create();
+    private static final Logger LOGGER = LoggerFactory.getLogger(Jsons.class);
+    public static final Gson GSON = new GsonBuilder().create();
 
-	public static String toJson(Object bean) {
+    public static String toJson(Object bean) {
 
-		try {
-			return GSON.toJson(bean);
-		} catch (Exception e) {
-			LOGGER.error("Jsons.toJson ex, bean=" + bean, e);
-		}
-		return null;
-	}
+        try {
+            return GSON.toJson(bean);
+        } catch (Exception e) {
+            LOGGER.error("Jsons.toJson ex, bean=" + bean, e);
+        }
+        return null;
+    }
 
-	public static <T> T fromJson(String json, Class<T> clazz) {
+    public static <T> T fromJson(String json, Class<T> clazz) {
 
-		try {
-			return GSON.fromJson(json, clazz);
-		} catch (Exception e) {
-			LOGGER.error("Jsons.fromJson ex, json=" + json + ", clazz=" + clazz, e);
-		}
-		return null;
-	}
+        try {
+            return GSON.fromJson(json, clazz);
+        } catch (Exception e) {
+            LOGGER.error("Jsons.fromJson ex, json=" + json + ", clazz=" + clazz, e);
+        }
+        return null;
+    }
 
-	public static <T> T fromJson(byte[] json, Class<T> clazz) {
-		return fromJson(new String(json, Constants.UTF_8), clazz);
-	}
+    public static <T> T fromJson(byte[] json, Class<T> clazz) {
+        return fromJson(new String(json, Constants.UTF_8), clazz);
+    }
 
-	public static <T> List<T> fromJsonToList(String json, Class<T[]> type) {
-		T[] list = GSON.fromJson(json, type);
-		return Arrays.asList(list);
-	}
+    public static <T> List<T> fromJsonToList(String json, Class<T[]> type) {
+        try {
+            T[] list = GSON.fromJson(json, type);
+            if (list == null) return null;
+            return Arrays.asList(list);
+        } catch (Exception e) {
+            LOGGER.error("Jsons.fromJsonToList ex, json=" + json + ", type=" + type, e);
+        }
+        return null;
+    }
 
-	public static <T> T fromJson(String json, Type type) {
-		try {
-			return GSON.fromJson(json, type);
-		} catch (Exception e) {
-			LOGGER.error("Jsons.fromJson ex, json=" + json + ", type=" + type, e);
-		}
-		return null;
-	}
+    public static <T> T fromJson(String json, Type type) {
+        try {
+            return GSON.fromJson(json, type);
+        } catch (Exception e) {
+            LOGGER.error("Jsons.fromJson ex, json=" + json + ", type=" + type, e);
+        }
+        return null;
+    }
 
-	public static boolean mayJson(String json) {
-		if (Strings.isBlank(json))
-			return false;
-		if (json.charAt(0) == '{' && json.charAt(json.length() - 1) == '}')
-			return true;
-		if (json.charAt(0) == '[' && json.charAt(json.length() - 1) == ']')
-			return true;
-		return false;
-	}
+    public static boolean mayJson(String json) {
+        if (Strings.isBlank(json))
+            return false;
+        if (json.charAt(0) == '{' && json.charAt(json.length() - 1) == '}')
+            return true;
+        if (json.charAt(0) == '[' && json.charAt(json.length() - 1) == ']')
+            return true;
+        return false;
+    }
 
-	public static String toJson(Map<String, String> map) {
-		if (map == null || map.isEmpty())
-			return "{}";
-		StringBuilder sb = new StringBuilder(64 * map.size());
-		sb.append('{');
-		Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
-		if (it.hasNext()) {
-			append(it.next(), sb);
-		}
-		while (it.hasNext()) {
-			sb.append(',');
-			append(it.next(), sb);
-		}
-		sb.append('}');
-		return sb.toString();
-	}
+    public static String toJson(Map<String, String> map) {
+        if (map == null || map.isEmpty())
+            return "{}";
+        StringBuilder sb = new StringBuilder(64 * map.size());
+        sb.append('{');
+        Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
+        if (it.hasNext()) {
+            append(it.next(), sb);
+        }
+        while (it.hasNext()) {
+            sb.append(',');
+            append(it.next(), sb);
+        }
+        sb.append('}');
+        return sb.toString();
+    }
 
-	private static void append(Map.Entry<String, String> entry, StringBuilder sb) {
-		String key = entry.getKey(), value = entry.getValue();
-		if (value == null)
-			value = Strings.EMPTY;
-		sb.append('"').append(key).append('"');
-		sb.append(':');
-		sb.append('"').append(value).append('"');
-	}
+    private static void append(Map.Entry<String, String> entry, StringBuilder sb) {
+        String key = entry.getKey(), value = entry.getValue();
+        if (value == null)
+            value = Strings.EMPTY;
+        sb.append('"').append(key).append('"');
+        sb.append(':');
+        sb.append('"').append(value).append('"');
+    }
 
-	public static void main(String[] args) {
-		String test = "test";
-		String ret = Jsons.toJson(test);
-		String ret2 = Jsons.fromJson(ret, String.class);
-		System.out.println(ret);
-		System.out.println(ret2);
-	}
+    public static void main(String[] args) {
+        String test = "test";
+        String ret = Jsons.toJson(test);
+        String ret2 = Jsons.fromJson(ret, String.class);
+        System.out.println(ret);
+        System.out.println(ret2);
+    }
 }
--- mpush-tools/src/main/java/com/shinemo/mpush/tools/zk/listener/impl/RedisPathListener.java
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/zk/listener/impl/RedisPathListener.java
@@ -1,7 +1,9 @@
 package com.shinemo.mpush.tools.zk.listener.impl;
 
+import java.util.Collections;
 import java.util.List;
 
+import com.google.common.base.Strings;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.curator.framework.CuratorFramework;
@@ -21,56 +23,55 @@ import com.shinemo.mpush.tools.zk.listener.CallBack;
 import com.shinemo.mpush.tools.zk.manage.ServerManage;
 
 /**
- *注册的应用的发生变化 
- *
+ * 注册的应用的发生变化
  */
-public class RedisPathListener implements CallBack{
-	
-	private static final Logger log = LoggerFactory.getLogger(RedisPathListener.class);
-	
-	@Override
-	public void handler(CuratorFramework client, TreeCacheEvent event, String path) {
-		String data = "";
-		if(event.getData()!=null){
-			data = ToStringBuilder.reflectionToString(event.getData(), ToStringStyle.MULTI_LINE_STYLE);
-		}
-		if (Type.NODE_ADDED == event.getType()) {
-			dataAddOrUpdate(event.getData());
-		} else if (Type.NODE_REMOVED == event.getType()) {
-			dataRemove(event.getData());
-		} else if (Type.NODE_UPDATED == event.getType()) {
-			dataAddOrUpdate(event.getData());
-		} else {
-			log.warn("ConnectionPathListener other path:" + path + "," + event.getType().name()+","+data);
-		}
-	}
+public class RedisPathListener implements CallBack {
 
-	@Override
-	public void initData(ServerManage manage) {
-		log.warn("start init redis data");
-		_initData();
-		log.warn("end init redis data");
-	}
-	
-	private void _initData(){
-		//获取redis列表
-		List<RedisGroup> group = getRedisGroup(PathEnum.CONNECTION_SERVER_REDIS.getPathByIp(InetAddressUtil.getInetAddress()));
-		RedisGroupManage.instance.init(group);
-	}
-	
-	private void dataRemove(ChildData data){
-		_initData();
-	}
-	
-	private void dataAddOrUpdate(ChildData data){
-		_initData();
-	}
-	
-	private List<RedisGroup> getRedisGroup(String fullPath){
-		String rawGroup = ZkUtil.instance.get(fullPath);
-		List<RedisGroup> group = Jsons.fromJsonToList(rawGroup, RedisGroup[].class);
-		return group;
-	}
+    private static final Logger log = LoggerFactory.getLogger(RedisPathListener.class);
 
+    @Override
+    public void handler(CuratorFramework client, TreeCacheEvent event, String path) {
+        String data = "";
+        if (event.getData() != null) {
+            data = ToStringBuilder.reflectionToString(event.getData(), ToStringStyle.MULTI_LINE_STYLE);
+        }
+        if (Type.NODE_ADDED == event.getType()) {
+            dataAddOrUpdate(event.getData());
+        } else if (Type.NODE_REMOVED == event.getType()) {
+            dataRemove(event.getData());
+        } else if (Type.NODE_UPDATED == event.getType()) {
+            dataAddOrUpdate(event.getData());
+        } else {
+            log.warn("ConnectionPathListener other path:" + path + "," + event.getType().name() + "," + data);
+        }
+    }
 
+    @Override
+    public void initData(ServerManage manage) {
+        log.warn("start init redis data");
+        _initData();
+        log.warn("end init redis data");
+    }
+
+    private void _initData() {
+        //获取redis列表
+        List<RedisGroup> group = getRedisGroup(PathEnum.CONNECTION_SERVER_REDIS.getPathByIp(InetAddressUtil.getInetAddress()));
+        RedisGroupManage.instance.init(group);
+    }
+
+    private void dataRemove(ChildData data) {
+        _initData();
+    }
+
+    private void dataAddOrUpdate(ChildData data) {
+        _initData();
+    }
+
+    private List<RedisGroup> getRedisGroup(String fullPath) {
+        String rawGroup = ZkUtil.instance.get(fullPath);
+        if (Strings.isNullOrEmpty(rawGroup)) return Collections.EMPTY_LIST;
+        List<RedisGroup> group = Jsons.fromJsonToList(rawGroup, RedisGroup[].class);
+        if (group == null) return Collections.EMPTY_LIST;
+        return group;
+    }
 }
