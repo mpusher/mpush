695c49bdb9cba3d2ffa31f40ca839e2e73282ab0
黄志磊 <huangzl@shinemo.com>
2016-01-13 11:45:11 +0800

    添加并发控制

--- mpush-tools/src/test/java/com/shinemo/mpush/tools/spi/SpiTest.java
+++ mpush-tools/src/test/java/com/shinemo/mpush/tools/spi/SpiTest.java
@@ -1,13 +1,42 @@
 package com.shinemo.mpush.tools.spi;
 
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
 import com.shinemo.mpush.tools.spi.test.TestService;
+
 
 public class SpiTest {
 	
-	public static void main(String[] args) {
+	private static Executor pool = Executors.newCachedThreadPool();
+	
+	@Ignore
+	@Test
+	public void baseTest(){
 		TestService testService = ServiceContainer.getInstance(TestService.class);
 		System.out.println(testService.sayHi(" huang"));
 	}
+	
+	@Test
+	public void mulThreadTest() throws InterruptedException{
+		pool.execute(new Worker());
+		pool.execute(new Worker());
+		Thread.sleep(Integer.MAX_VALUE);
+	}
+	
 
+	private static final class Worker implements Runnable{
+		
+		@Override
+		public void run() {
+			TestService testService = ServiceContainer.getInstance(TestService.class);
+			System.out.println(testService.sayHi(" huang"));
+		}
+		
+	}
+	
 }
