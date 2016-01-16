9e6666972f30cbda5fc93e47707ab78c0fb6aebd
黄志磊 <huangzl@shinemo.com>
2016-01-16 17:19:05 +0800

    add

--- mpush-cs/src/main/java/com/shinemo/mpush/cs/Main.java
+++ mpush-cs/src/main/java/com/shinemo/mpush/cs/Main.java
@@ -1,6 +1,11 @@
 package com.shinemo.mpush.cs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+	
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
 	
 	public static void main(String[] args) {
 		final ConnectionServerMain connectionServerMain = new ConnectionServerMain();
@@ -8,6 +13,7 @@ public class Main {
 		Runtime.getRuntime().addShutdownHook(new Thread() {
             public void run() {
             	connectionServerMain.stop();
+            	log.warn("connection stop success!");
             }
         });
 	}
