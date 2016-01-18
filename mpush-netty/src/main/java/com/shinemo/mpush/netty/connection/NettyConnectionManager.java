4981ed42aca6c4015ff54fbc093b7718c5af0461
黄志磊 <huangzl@shinemo.com>
2016-01-18 16:29:00 +0800

    remove unused

--- mpush-netty/src/main/java/com/shinemo/mpush/netty/connection/NettyConnectionManager.java
+++ mpush-netty/src/main/java/com/shinemo/mpush/netty/connection/NettyConnectionManager.java
@@ -18,10 +18,7 @@ import io.netty.util.internal.chmv8.ConcurrentHashMapV8;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
