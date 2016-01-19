42755f52ba020b3b3d88c858882fbfc669b812dc
黄志磊 <huangzl@shinemo.com>
2016-01-19 11:00:59 +0800

    remove

--- mpush-netty/src/main/java/com/shinemo/mpush/netty/util/NettySharedHolder.java
+++ mpush-netty/src/main/java/com/shinemo/mpush/netty/util/NettySharedHolder.java
@@ -3,9 +3,6 @@ package com.shinemo.mpush.netty.util;
 
 import com.shinemo.mpush.tools.thread.NamedThreadFactory;
 import com.shinemo.mpush.tools.thread.ThreadNameSpace;
-
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.util.HashedWheelTimer;
 import io.netty.util.Timer;
 
