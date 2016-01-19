adb62a03d3961183a7830066fc3d361d1fcda625
黄志磊 <huangzl@shinemo.com>
2016-01-19 11:48:49 +0800

    tostring

--- mpush-api/src/main/java/com/shinemo/mpush/api/connection/SessionContext.java
+++ mpush-api/src/main/java/com/shinemo/mpush/api/connection/SessionContext.java
@@ -43,14 +43,10 @@ public final class SessionContext {
         return deviceId != null && deviceId.length() > 0;
     }
 
-    @Override
-    public String toString() {
-        return "SessionContext{" +
-                ", osName='" + osName + '\'' +
-                ", osVersion='" + osVersion + '\'' +
-                ", clientVersion='" + clientVersion + '\'' +
-                ", deviceId='" + deviceId + '\'' +
-                ", heartbeat=" + heartbeat +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "SessionContext [osName=" + osName + ", osVersion=" + osVersion + ", clientVersion=" + clientVersion + ", deviceId=" + deviceId + ", heartbeat=" + heartbeat + ", cipher=" + cipher
+				+ "]";
+	}
+
 }
