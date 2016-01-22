afed584a9463af8315be66b6ba50b36307c00b3d
黄志磊 <huangzl@shinemo.com>
2016-01-22 14:05:40 +0800

    client 重构

--- mpush-cs/src/main/java/com/shinemo/mpush/cs/client/Main.java
+++ mpush-cs/src/main/java/com/shinemo/mpush/cs/client/Main.java
@@ -3,8 +3,6 @@ package com.shinemo.mpush.cs.client;
 import java.util.List;
 import java.util.concurrent.locks.LockSupport;
 
-import com.google.common.collect.Lists;
-import com.shinemo.mpush.api.Client;
 import com.shinemo.mpush.core.client.ClientChannelHandler;
 import com.shinemo.mpush.cs.ConnectionServerApplication;
 import com.shinemo.mpush.netty.client.NettyClientFactory;
@@ -21,9 +19,18 @@ public class Main {
 		int index = (int) ((Math.random() % serverList.size()) * serverList.size());
 		ConnectionServerApplication server = serverList.get(index);
 		
+		byte[] clientKey = null;
+		byte[] iv = null;
+		String clientVersion = null;
+        String deviceId = null;
+        String osName = null;
+        String osVersion = null;
+        String userId = null;
+        String cipher = null;
+		
 		for(int i = 0;i<100;i++){
 			ClientChannelHandler handler = new ClientChannelHandler();
-			NettyClientFactory.INSTANCE.createClient(server.getIp(), server.getPort(), handler, true);
+			NettyClientFactory.INSTANCE.createSecurityClient(server.getIp(), server.getPort(), handler, clientKey, iv, clientVersion, deviceId, osName, osVersion, userId, cipher);
 		}
 		
 		LockSupport.park();
