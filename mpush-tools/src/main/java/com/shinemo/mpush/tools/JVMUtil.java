67ed534b76a5691906e36f8e65c167d332f4fe55
黄志磊 <huangzl@shinemo.com>
2016-01-26 13:49:20 +0800

    add jmap

--- mpush-tools/src/main/java/com/shinemo/mpush/tools/JVMUtil.java
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/JVMUtil.java
@@ -5,16 +5,30 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.management.ManagementFactory;
+import java.security.AccessController;
+import java.security.PrivilegedExceptionAction;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Executors;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.management.HotSpotDiagnosticMXBean;
+
 public class JVMUtil {
 	
 	private static final Logger log = LoggerFactory.getLogger(JVMUtil.class);
+	
+	private static final String HOTSPOT_BEAN_NAME = "com.sun.management:type=HotSpotDiagnostic";
+    private static volatile HotSpotDiagnosticMXBean hotspotMBean;
+    
+    private static Object lock = new Object();
 	
 	public static void jstack(OutputStream stream) throws Exception {
         try {
@@ -45,7 +59,7 @@ public class JVMUtil {
                 String logPath = jvmPath;
                 FileOutputStream jstackStream = null;
                 try {
-                    jstackStream = new FileOutputStream(new File(logPath, "jstack.log"));
+                    jstackStream = new FileOutputStream(new File(logPath, System.currentTimeMillis()+"-jstack.log"));
                     JVMUtil.jstack(jstackStream);
                 } catch (FileNotFoundException e) {
                 	log.error("", "Dump JVM cache Error!", e);
@@ -61,6 +75,69 @@ public class JVMUtil {
                 }
             }
         });
+	}
+	
+    private static HotSpotDiagnosticMXBean getHotspotMBean() {
+        try {
+            return AccessController.doPrivileged(new PrivilegedExceptionAction<HotSpotDiagnosticMXBean>() {
+                public HotSpotDiagnosticMXBean run() throws Exception {
+                    MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+                    Set<ObjectName> s = server.queryNames(new ObjectName(HOTSPOT_BEAN_NAME), null);
+                    Iterator<ObjectName> itr = s.iterator();
+                    if (itr.hasNext()) {
+                        ObjectName name = itr.next();
+                        HotSpotDiagnosticMXBean bean = ManagementFactory.newPlatformMXBeanProxy(server,
+                                name.toString(), HotSpotDiagnosticMXBean.class);
+                        return bean;
+                    } else {
+                        return null;
+                    }
+                }
+            });
+        } catch (Exception e) {
+            log.error("", "getHotspotMBean Error!", e);
+            return null;
+        }
+    }
+    
+    private static void initHotspotMBean() throws Exception {
+        if (hotspotMBean == null) {
+            synchronized (lock) {
+                if (hotspotMBean == null) {
+                    hotspotMBean = getHotspotMBean();
+                }
+            }
+        }
+    }
+    
+    public static void jMap(String fileName, boolean live) {
+        try {
+            initHotspotMBean();
+            
+
+            
+            File f = new File(fileName, System.currentTimeMillis()+"-jmap.log");
+            String currentFileName = f.getPath();
+            if (f.exists()) {
+                f.delete();
+            }
+            
+            
+            hotspotMBean.dumpHeap(currentFileName, live);
+        } catch (Exception e) {
+        	log.error("", "dumpHeap Error!", e);
+        }
+    }
+	
+	public static void dumpJmap(final String jvmPath){
+		
+		Executors.newSingleThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+            	jMap(jvmPath, false);
+            }
+        });
+		
 	}
 
 }
