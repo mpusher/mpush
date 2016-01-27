8bcce8078ffea5bb5f8b2cf7a3ec1d7c3e995b7d
黄志磊 <huangzl@shinemo.com>
2016-01-27 15:10:54 +0800

    add thread pool test

--- mpush-tools/src/main/java/com/shinemo/mpush/tools/thread/threadpool/cached/CachedThreadPoolContext.java
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/thread/threadpool/cached/CachedThreadPoolContext.java
@@ -5,11 +5,19 @@ import com.shinemo.mpush.tools.thread.threadpool.ThreadPoolContext;
 public class CachedThreadPoolContext extends ThreadPoolContext{
 
 	public CachedThreadPoolContext(String name, int corePoolSize, int maxPoolSize, int keepAliveSeconds) {
-		super(name, corePoolSize, maxPoolSize, 0, keepAliveSeconds);
+		this(name, corePoolSize, maxPoolSize, keepAliveSeconds, 0);
+	}
+	
+	public CachedThreadPoolContext(String name, int corePoolSize, int maxPoolSize, int keepAliveSeconds,int queueCapacity) {
+		super(name, corePoolSize, maxPoolSize, queueCapacity, keepAliveSeconds);
 	}
 	
 	public static CachedThreadPoolContext create(String name, int corePoolSize, int maxPoolSize, int keepAliveSeconds){
 		return new CachedThreadPoolContext(name, corePoolSize, maxPoolSize, keepAliveSeconds);
+	}
+	
+	public static CachedThreadPoolContext create(String name, int corePoolSize, int maxPoolSize, int keepAliveSeconds,int queueCapacity){
+		return new CachedThreadPoolContext(name, corePoolSize, maxPoolSize, keepAliveSeconds,queueCapacity);
 	}
 
 }
