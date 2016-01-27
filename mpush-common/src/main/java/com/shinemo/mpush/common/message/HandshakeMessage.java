8201fd9cee73e44019c9d84b6c56ad3df37d19a2
黄志磊 <huangzl@shinemo.com>
2016-01-27 16:26:34 +0800

    加密可能报异常，需要接下来处理

--- mpush-common/src/main/java/com/shinemo/mpush/common/message/HandshakeMessage.java
+++ mpush-common/src/main/java/com/shinemo/mpush/common/message/HandshakeMessage.java
@@ -1,7 +1,6 @@
 package com.shinemo.mpush.common.message;
 
 import com.shinemo.mpush.api.connection.Connection;
-import com.shinemo.mpush.api.protocol.Command;
 import com.shinemo.mpush.api.protocol.Packet;
 import io.netty.buffer.ByteBuf;
 
