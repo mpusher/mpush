8a9230c141cc8d15ed70b26d56868e4a890d9abe
黄志磊 <huangzl@shinemo.com>
2016-01-27 19:50:57 +0800

    add exception

---
+++ mpush-api/src/main/java/com/shinemo/mpush/api/exception/MessageException.java
@@ -0,0 +1,11 @@
+package com.shinemo.mpush.api.exception;
+
+public class MessageException extends RuntimeException {
+	
+	private static final long serialVersionUID = 8731698346169093329L;
+
+    public MessageException(String message) {
+        super(message);
+    }
+	
+}
