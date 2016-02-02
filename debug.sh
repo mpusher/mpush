ba43c6221ddc3d1feba23d6e4225a498061f836d
黄志磊 <huangzl@shinemo.com>
2016-02-02 15:24:04 +0800

    add

--- debug.sh
+++ debug.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+ENV=daily
+
+base_dir=`pwd`
+
 echo "start assembly lib..."
-mvn assembly:assembly
-tar -xzvf ./target/mpush-jar-with-dependency.tar.gz
+mvn clean assembly:assembly -P $ENV
+
+echo "start tar mpush..."
+cd $base_dir/target
+tar -xzvf ./mpush-jar-with-dependency.tar.gz
 echo "start start mpush..."
-java -jar ./target/mpush/mpush-cs.jar  -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n &
+
+java -jar $base_dir/target/mpush/mpush-cs.jar  -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n &
 
 echo "end start mpush..."
