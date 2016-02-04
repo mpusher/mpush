d42aba7bcc6b5a78b59ed6ca22671c2d3ca0ee01
黄志磊 <huangzl@shinemo.com>
2016-02-04 11:30:48 +0800

    add test module

---
+++ mpush-test/src/main/java/com/shinemo/mpush/test/push/zk/listener/impl/GatewayServerPathListener.java
@@ -0,0 +1,40 @@
+package com.shinemo.mpush.test.push.zk.listener.impl;
+
+import com.shinemo.mpush.common.app.impl.GatewayServerApplication;
+import com.shinemo.mpush.common.manage.ServerManage;
+import com.shinemo.mpush.common.zk.listener.AbstractDataChangeListener;
+import com.shinemo.mpush.tools.spi.ServiceContainer;
+import com.shinemo.mpush.tools.zk.ZKPath;
+
+/**
+ * gateway server 应用  监控
+ * 
+ */
+public class GatewayServerPathListener extends AbstractDataChangeListener<GatewayServerApplication>{
+	
+	@SuppressWarnings("unchecked")
+	private ServerManage<GatewayServerApplication> gatewayServerManage = ServiceContainer.getInstance(ServerManage.class, "gatewayServerManage");
+	
+	@Override
+	public String listenerPath() {
+		return ZKPath.GATEWAY_SERVER.getWatchPath();
+	}
+
+	@Override
+	public ServerManage<GatewayServerApplication> getServerManage() {
+		return gatewayServerManage;
+	}
+
+	@Override
+	public String getRegisterPath() {
+		return ZKPath.GATEWAY_SERVER.getPath();
+	}
+
+	@Override
+	public String getFullPath(String raw) {
+		return ZKPath.GATEWAY_SERVER.getFullPath(raw);
+	}
+
+	
+
+}
