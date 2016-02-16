8485e7fba4a0d4a762e750433987fba4c4bf43fd
夜色 <ohun@live.cn>
2016-02-16 15:41:46 +0000

    add http proxy

--- mpush-common/src/main/java/com/shinemo/mpush/common/handler/BaseMessageHandler.java
+++ mpush-common/src/main/java/com/shinemo/mpush/common/handler/BaseMessageHandler.java
@@ -1,11 +1,9 @@
 package com.shinemo.mpush.common.handler;
 
 
-import com.shinemo.mpush.api.Constants;
 import com.shinemo.mpush.api.Message;
 import com.shinemo.mpush.api.MessageHandler;
 import com.shinemo.mpush.api.connection.Connection;
-import com.shinemo.mpush.api.exception.CryptoException;
 import com.shinemo.mpush.api.protocol.Packet;
 
 /**
@@ -17,19 +15,9 @@ public abstract class BaseMessageHandler<T extends Message> implements MessageHa
     public abstract void handle(T message);
 
     public void handle(Packet packet, Connection connection) {
-    	if(checkCrypto(packet)){
-    		T t = decode(packet, connection);
-            if (t != null) {
-                handle(t);
-            }
-    	}
-    }
-    
-    public boolean checkCrypto(Packet packet){
-    	if(packet.hasFlag(Constants.CRYPTO_FLAG)){
-    		return true;
-    	}else{
-    		throw new CryptoException("need crypto");
-    	}
+        T t = decode(packet, connection);
+        if (t != null) {
+            handle(t);
+        }
     }
 }
