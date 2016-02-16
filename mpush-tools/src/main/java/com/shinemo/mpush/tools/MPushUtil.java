7affdf6242e6bf3d8c654019410a250e2de246f3
闫逍旭 <yanxx@shinemo.com>
2016-02-16 15:23:53 +0000

    add http proxy

--- mpush-tools/src/main/java/com/shinemo/mpush/tools/MPushUtil.java
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/MPushUtil.java
@@ -8,6 +8,8 @@ import com.shinemo.mpush.tools.config.ConfigCenter;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ohun on 2015/12/25.
@@ -57,5 +59,41 @@ public final class MPushUtil {
             LOGGER.error("getInetAddress exception", e);
             return "127.0.0.1";
         }
+    }
+
+    public static String headerToString(Map<String, String> headers) {
+        if (headers != null && headers.size() > 0) {
+            StringBuilder sb = new StringBuilder(headers.size() * 64);
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                sb.append(entry.getKey())
+                        .append(':')
+                        .append(entry.getValue()).append('\n');
+            }
+            return sb.toString();
+        }
+        return null;
+    }
+
+
+    public static Map<String, String> headerFromString(String headersString) {
+        if (headersString == null) return null;
+        Map<String, String> headers = new HashMap<>();
+        int L = headersString.length();
+        String name, value = null;
+        for (int i = 0, start = 0; i < L; i++) {
+            char c = headersString.charAt(i);
+            if (c != '\n') continue;
+            if (start >= L - 1) break;
+            String header = headersString.substring(start, i);
+            start = i + 1;
+            int index = header.indexOf(':');
+            if (index <= 0) continue;
+            name = header.substring(0, index);
+            if (index < header.length() - 1) {
+                value = header.substring(index + 1);
+            }
+            headers.put(name, value);
+        }
+        return headers;
     }
 }
