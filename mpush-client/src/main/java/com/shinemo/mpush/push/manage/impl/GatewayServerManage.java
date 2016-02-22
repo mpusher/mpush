6e8553585dfb98da8359baf773fa207b7e72ae4a
黄志磊 <huangzl@shinemo.com>
2016-02-22 12:44:26 +0800

    add sleep

--- mpush-client/src/main/java/com/shinemo/mpush/push/manage/impl/GatewayServerManage.java
+++ mpush-client/src/main/java/com/shinemo/mpush/push/manage/impl/GatewayServerManage.java
@@ -34,7 +34,7 @@ public class GatewayServerManage implements ServerManage<GatewayServerApplicatio
 		try{
 			Client client = new NettyClient(application.getIp(), application.getPort());
 			ClientChannelHandler handler = new ClientChannelHandler(client);
-			Thread.sleep(30);
+			Thread.sleep(250);
 			NettyClientFactory.INSTANCE.create(handler);
 			application2Client.put(application, client);
 			ip2Client.put(application.getIp(), client);
