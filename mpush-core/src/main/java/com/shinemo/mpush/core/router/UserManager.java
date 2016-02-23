09f4771df4012d36aedeb3788a37719486c97e53
黄志磊 <huangzl@shinemo.com>
2016-02-23 15:11:38 +0800

    pub sub user online offline

--- mpush-core/src/main/java/com/shinemo/mpush/core/router/UserManager.java
+++ mpush-core/src/main/java/com/shinemo/mpush/core/router/UserManager.java
@@ -4,6 +4,7 @@ import com.google.common.eventbus.Subscribe;
 import com.shinemo.mpush.api.event.UserOfflineEvent;
 import com.shinemo.mpush.api.event.UserOnlineEvent;
 import com.shinemo.mpush.common.EventBus;
+import com.shinemo.mpush.tools.redis.manage.RedisManage;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -15,6 +16,10 @@ import org.slf4j.LoggerFactory;
 public final class UserManager extends com.shinemo.mpush.common.manage.user.UserManager{
     public static final Logger LOGGER = LoggerFactory.getLogger(UserManager.class);
 
+    public static final String ONLINE_CHANNEL = "/mpush/online/";
+    
+    public static final String OFFLINE_CHANNEL = "/mpush/offline/";
+    
     public UserManager() {
     	EventBus.INSTANCE.register(this);
 	}
@@ -22,6 +27,7 @@ public final class UserManager extends com.shinemo.mpush.common.manage.user.User
     @Subscribe
     void handlerUserOnlineEvent(UserOnlineEvent event) {
     	userOnline(event.getUserId());
+    	RedisManage.publish(ONLINE_CHANNEL, event.getUserId());
     }
     
     @Subscribe
@@ -30,6 +36,7 @@ public final class UserManager extends com.shinemo.mpush.common.manage.user.User
     		String userId = RouterCenter.INSTANCE.getLocalRouterManager().getUserIdByConnId(event.getConnection().getId());
     		if(StringUtils.isNotBlank(userId)){
     			userOffline(userId);
+    			RedisManage.publish(OFFLINE_CHANNEL, event.getUserId());
     		}
     	}else{ //解绑用户
     		userOffline(event.getUserId());
