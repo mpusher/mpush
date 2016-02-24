48d59f14e3d541fbc694e381826f494eb7812886
黄志磊 <huangzl@shinemo.com>
2016-02-24 17:44:15 +0800

    add bash

---
+++ pub-hive.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ENV=dev
+
+base_dir=`pwd`
+
+echo "start assembly lib..."
+
+rm -rf $base_dir/target
+
+mvn clean install  assembly:assembly -P $ENV
+
+echo "start scp mpush..."
+
+cd $base_dir/target
+
+scp -P 9092 ./mpush-jar-with-dependency.tar.gz hive1_host:~/mpush
+
+scp -P 9092 ./mpush-jar-with-dependency.tar.gz hive2_host:~/mpush
