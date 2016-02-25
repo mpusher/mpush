3ffdc7f8ccb6afa1b3c2a0d7419067208fae7052
黄志磊 <huangzl@shinemo.com>
2016-02-25 17:32:43 +0800

    add num

--- mpush-core/src/main/java/com/shinemo/mpush/core/router/UserManager.java
+++ mpush-core/src/main/java/com/shinemo/mpush/core/router/UserManager.java
@@ -1,12 +1,12 @@
 package com.shinemo.mpush.core.router;
 
 import com.google.common.eventbus.Subscribe;
+import com.shinemo.mpush.api.RedisKey;
 import com.shinemo.mpush.api.event.UserOfflineEvent;
 import com.shinemo.mpush.api.event.UserOnlineEvent;
 import com.shinemo.mpush.common.EventBus;
+import com.shinemo.mpush.tools.MPushUtil;
 import com.shinemo.mpush.tools.redis.manage.RedisManage;
-
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +19,10 @@ public final class UserManager extends com.shinemo.mpush.common.manage.user.User
     public static final String ONLINE_CHANNEL = "/mpush/online/";
 
     public static final String OFFLINE_CHANNEL = "/mpush/offline/";
+    
+    private static final String EXTRANET_ADDRESS = MPushUtil.getExtranetAddress();
+    
+    private static final String ONLINE_KEY = RedisKey.getConnNum(EXTRANET_ADDRESS); 
 
     public UserManager() {
         EventBus.INSTANCE.register(this);
@@ -28,18 +32,26 @@ public final class UserManager extends com.shinemo.mpush.common.manage.user.User
     void handlerUserOnlineEvent(UserOnlineEvent event) {
         userOnline(event.getUserId());
         RedisManage.publish(ONLINE_CHANNEL, event.getUserId());
+        RedisManage.incrBy(ONLINE_KEY, 1);
     }
 
     @Subscribe
     void handlerUserOfflineEvent(UserOfflineEvent event) {
-        if (event.getUserId() == null) {//链接超时
-            String userId = RouterCenter.INSTANCE.getLocalRouterManager().getUserIdByConnId(event.getConnection().getId());
-            if (StringUtils.isNotBlank(userId)) {
-                userOffline(userId);
-                RedisManage.publish(OFFLINE_CHANNEL, event.getUserId());
-            }
-        } else { //解绑用户
-            userOffline(event.getUserId());
-        }
+//        if (event.getUserId() == null) {//链接超时
+//            String userId = RouterCenter.INSTANCE.getLocalRouterManager().getUserIdByConnId(event.getConnection().getId());
+//            if (StringUtils.isNotBlank(userId)) {
+//                userOffline(userId);
+//                RedisManage.publish(OFFLINE_CHANNEL, event.getUserId());
+//            }
+//        } else { //解绑用户
+//            userOffline(event.getUserId());
+//            RedisManage.publish(OFFLINE_CHANNEL, event.getUserId());
+//        }
+    	if(event.getUserId()==null){
+    		return;
+    	}
+    	userOffline(event.getUserId());
+        RedisManage.publish(OFFLINE_CHANNEL, event.getUserId());
+        RedisManage.incrBy(ONLINE_KEY, -1);
     }
 }
