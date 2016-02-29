8e6bc1a7b6f309a593c064f9859aa52ac0dcb997
黄志磊 <huangzl@shinemo.com>
2016-02-29 20:40:25 +0800

    add python

--- pub-python.py
+++ pub-python.py
@@ -25,6 +25,8 @@ STARTPROCESS = 'java -jar mpush-cs.jar'
 
 GITLABPATH = '/data/localgit/mpush/mpush/target/mpush-jar-with-dependency.tar.gz'
 
+ENV=daily
+
 
 class SSH():
     def __init__(self):
@@ -44,6 +46,7 @@ class SSH():
         if isprint:
             for std in stdout.readlines():
                 print std,
+        print stderr.read()
         return stdin, stdout, stderr
 
 
@@ -52,7 +55,7 @@ class SSH():
             self.client.close()
 
 def getPid(ssh):
-    stdin, stdout, stderr = ssh.exe(''' ps aux|grep "java -jar mpush-cs.jar" |grep -v "grep"|awk '{print $2}' ''',False)
+    stdin, stdout, stderr = ssh.exe(''' ps aux|grep "mpush-cs.jar" |grep -v "grep"|awk '{print $2}' ''',False)
     return stdout.read().strip()
 def showText(s, typ):
     if typ == 'RED':
@@ -83,13 +86,14 @@ def main():
         ssh = SSH().connect(item['HOST'],item['PORT'],username=item['USER'])
 
         ##0 assembly
-
+        runShell('mvn clean install  assembly:assembly -P %s'%DEV))
+        print showText('assembly success','greenText')
 
         ##1 backup
         base = BASEPATH+'/mpush-jar-with-dependency.tar.gz'
         to = BASEPATH+'/back/mpush-jar-with-dependency.tar.gz.'+datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        ssh.exe('cp %s %s '%(base,to))
-        print greenText('backup mpush ok')
+        ssh.exe('mv %s %s '%(base,to))
+        print showText('backup mpush ok','greenText')
 
         ##telnet remove zk  info
         #ssh.exe('telent 127.0.0.1 4001')
@@ -97,21 +101,23 @@ def main():
 
         ##2 kill process
         pid = getPid(ssh)
-        print '=============',pid
 
         if pid :
             ssh.exe('kill -9 %s'%pid)
         else:
-            print yellowText('there is no mpush-cs process to kill')
+            print showText('there is no mpush-cs process to kill','YELLOW')
 
         ##3 scp
-        runShell('scp -P %s %s %s:%s'%(item['PORT'],GITLABPATH,item['HOST'],'/root/mpush'))
+        runShell('scp -P %s %s %s:%s'%(item['PORT'],GITLABPATH,item['HOST'],'/root/mpush'),False)
+        print showText('scp success','greenText')
 
         ##4  tar package
-        ssh.exe('cd /root/mpush && tar -xzvf ./mpush-jar-with-dependency.tar.gz')
+        ssh.exe('cd /root/mpush/ && tar -xzvf ./mpush-jar-with-dependency.tar.gz')
+        print showText('tar success','greenText')
 
         ##5 start process
-        ssh.exe('java -jar /root/mpush/mpush/mpush-cs.jar &')
+        ssh.exe('/opt/shinemo/jdk1.7.0_40/bin/java -jar /root/mpush/mpush/mpush-cs.jar &')
+        print showText('start process success','greenText')
 
         ssh.close()
 
