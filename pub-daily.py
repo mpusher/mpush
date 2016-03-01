b856fd8f70119152584a630e674ce167c527813a
黄志磊 <huangzl@shinemo.com>
2016-03-01 10:40:12 +0800

    add

---
+++ pub-daily.py
@@ -0,0 +1,140 @@
+# coding=utf8
+
+import paramiko
+import datetime
+import telnetlib
+import os
+import sys
+
+HOSTS = [
+    {
+        'HOST':'hive1_host',
+        'PORT':9092,
+        'USER':'root'
+    },
+    {
+        'HOST':'hive2_host',
+        'PORT':9092,
+        'USER':'root'
+    }
+]
+
+BASEPATH = '/root/mpush'
+
+MPUSH_TAR_NAME = 'mpush-jar-with-dependency.tar.gz'
+
+PROCESS_KEY_WORD = 'mpush-cs.jar'
+
+GITLABPATH = '/data/localgit/mpush/mpush/target/'+MPUSH_TAR_NAME
+
+JAVA_PATH = '/opt/shinemo/jdk1.7.0_40/bin/java'
+
+ENV= 'daily'
+
+
+class SSH():
+    def __init__(self):
+        self.client = None
+
+    def connect(self,host,port=22,username='root',password=None):
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.client.connect(host, port, username=username, password=password, timeout=120)
+        return self
+
+    def exe(self,cmd,isprint=True,get_pty=True):
+        if not cmd:
+            return
+        print greenText(cmd)
+        stdin, stdout, stderr = self.client.exec_command(cmd,get_pty=get_pty)
+        if isprint:
+            for std in stdout.readlines():
+                print std,
+        print stderr.read()
+        return stdin, stdout, stderr
+
+
+    def close(self):
+        if self.client:
+            self.client.close()
+
+def getPid(ssh):
+    stdin, stdout, stderr = ssh.exe(' ps aux|grep %s |grep -v "grep"|awk \'{print $2}\' '%PROCESS_KEY_WORD,False)
+    return stdout.read().strip()
+def showText(s, typ):
+    if typ == 'RED':
+        return redText(s)
+    elif typ == 'GREEN':
+        return greenText(s)
+    elif typ == 'YELLOW':
+        return yellowText(s)
+    else:
+        return s
+
+def redText(s):
+    return "\033[1;31m%s\033[0m" % s
+
+def greenText(s):
+    return "\033[1;32m%s\033[0m" % s
+
+
+def yellowText(s):
+    return "\033[1;33m%s\033[0m" % s
+
+def runShell(c):
+    print c
+    os.system(c)
+
+def main():
+
+    ##0 assembly
+    runShell('mvn clean install  assembly:assembly -P %s'%ENV)
+    print showText('assembly success','greenText')
+
+    ##1 包创建时间
+    runShell('stat -c "%%y" %s'%GITLABPATH)
+
+    confirmPub = raw_input("确认发布(Y/N)：")
+
+    if confirmPub != 'Y':
+       return
+
+    for item in HOSTS:
+
+        pubHost = raw_input("发布 %s (Y/N)："%item['HOST'])
+        if pubHost != 'Y':
+           return
+
+        ssh = SSH().connect(item['HOST'],item['PORT'],username=item['USER'])
+
+        ##2 backup
+        base = BASEPATH+MPUSH_TAR_NAME
+        to = BASEPATH+MPUSH_TAR_NAME+'.'+datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        ssh.exe('mv %s %s '%(base,to))
+        print showText('backup mpush ok','greenText')
+
+        ##3 kill process
+        pid = getPid(ssh)
+        if pid :
+            ssh.exe('kill -9 %s'%pid)
+        else:
+            print showText('there is no process to kill','YELLOW')
+
+        ##4 scp
+        runShell('scp -P %s %s %s:%s'%(item['PORT'],GITLABPATH,item['HOST'],'/root/mpush'))
+        print showText('scp success','greenText')
+
+        ##5  tar package
+        ssh.exe('cd %s && tar -xzvf ./%s'%(BASEPATH,MPUSH_TAR_NAME),False)
+        print showText('tar success','greenText')
+
+        ##6 start process
+        ssh.exe('nohup %s -jar %s/mpush/mpush-cs.jar >> %s/mpush/nohup.out 2>&1 &'%(JAVA_PATH,BASEPATH,BASEPATH))
+        print showText('start process success','greenText')
+
+
+        ssh.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# coding=utf8
+
+import datetime
+import os
+import sys
+
+str = raw_input("请输入：");
+print "你输入的内容是: ", str
