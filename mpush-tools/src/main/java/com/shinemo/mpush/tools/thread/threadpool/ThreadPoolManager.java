7261a27eb702b4e8d4d53efe42bd151cfa638f58
黄志磊 <huangzl@shinemo.com>
2016-03-08 15:11:28 +0800

    bug fix

--- mpush-tools/src/main/java/com/shinemo/mpush/tools/thread/threadpool/ThreadPoolManager.java
+++ mpush-tools/src/main/java/com/shinemo/mpush/tools/thread/threadpool/ThreadPoolManager.java
@@ -21,7 +21,7 @@ public class ThreadPoolManager {
 	public static Executor eventBusExecutor = fixedThreadPool.getExecutor(ThreadPoolContext.EVENT_BUS_THREAD_POOL);
 	public static Executor redisExecutor = fixedThreadPool.getExecutor(ThreadPoolContext.REDIS_THREAD_POOL);
 	
-	public static Executor httpExecutor = cachedThreadPool.getExecutor(ThreadPoolContext.HTTP_THREAD_POOL);
+	public static Executor httpExecutor = fixedThreadPool.getExecutor(ThreadPoolContext.HTTP_THREAD_POOL);
 	
 	static{
 		poolCache.put(ThreadPoolContext.BOSS_THREAD_POOL.getName(), bossExecutor);
