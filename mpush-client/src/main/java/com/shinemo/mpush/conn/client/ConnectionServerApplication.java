2851c08011a2e2880d9032f8aaab819338eb8ed2
夜色 <ohun@live.cn>
2016-05-13 10:27:30 +0200

    增加本机IP到外网IP的映射配置

--- mpush-client/src/main/java/com/shinemo/mpush/conn/client/ConnectionServerApplication.java
+++ mpush-client/src/main/java/com/shinemo/mpush/conn/client/ConnectionServerApplication.java
@@ -1,32 +1,31 @@
 package com.shinemo.mpush.conn.client;
 
 import com.shinemo.mpush.common.app.Application;
-import com.shinemo.mpush.conn.client.GatewayServerApplication;
 import com.shinemo.mpush.tools.MPushUtil;
 import com.shinemo.mpush.tools.config.ConfigCenter;
 import com.shinemo.mpush.tools.zk.ZKPath;
+import org.slf4j.Logger;
 
-public class ConnectionServerApplication extends Application{
-	
-	private transient GatewayServerApplication gatewayServerApplication;
-	
-	public ConnectionServerApplication() throws Exception {
-		this(ConfigCenter.holder.connectionServerPort(),ZKPath.CONNECTION_SERVER.getWatchPath(),MPushUtil.getLocalIp(),MPushUtil.getExtranetAddress());
-	}
-	
-	public ConnectionServerApplication(int port,String path,String ip,String extranetIp) {
-		setPort(port);
-		setServerRegisterZkPath(path);
-		setIp(ip);
-		setExtranetIp(extranetIp);
-	}
+public class ConnectionServerApplication extends Application {
+    private transient GatewayServerApplication gatewayServerApplication;
 
-	public GatewayServerApplication getGatewayServerApplication() {
-		return gatewayServerApplication;
-	}
+    public ConnectionServerApplication() throws Exception {
+        this(ConfigCenter.holder.connectionServerPort(), ZKPath.CONNECTION_SERVER.getWatchPath(), MPushUtil.getLocalIp(), MPushUtil.getExtranetAddress());
+    }
 
-	public void setGatewayServerApplication(GatewayServerApplication gatewayServerApplication) {
-		this.gatewayServerApplication = gatewayServerApplication;
-	}
+    public ConnectionServerApplication(int port, String path, String ip, String extranetIp) {
+        setPort(port);
+        setServerRegisterZkPath(path);
+        setIp(ip);
+        setExtranetIp(extranetIp);
+    }
+
+    public GatewayServerApplication getGatewayServerApplication() {
+        return gatewayServerApplication;
+    }
+
+    public void setGatewayServerApplication(GatewayServerApplication gatewayServerApplication) {
+        this.gatewayServerApplication = gatewayServerApplication;
+    }
 
 }
