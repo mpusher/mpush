c628dc8220b8f5a975e7050c0c23f65d80c2d104
maksim <154536744@qq.com>
2016-05-16 15:31:50 +0800

    gateway server should not open external ip port!
    gateway server should run within same network with connection server

--- mpush-zk/src/main/java/com/mpush/zk/ZKServerNode.java
+++ mpush-zk/src/main/java/com/mpush/zk/ZKServerNode.java
@@ -37,7 +37,7 @@ public class ZKServerNode {
     public static ZKServerNode gsNode() {
         return new ZKServerNode(MPushUtil.getLocalIp(),
                 ConfigCenter.I.gatewayServerPort(),
-                MPushUtil.getExtranetAddress(),
+                null,
                 ZKPath.GATEWAY_SERVER.getWatchPath());
     }
 
