8505f49365399cdd75282e0f05c89938e9da56f5
夜色 <yanxx@shinemo.com>
2016-10-15 11:35:44 +0800

    增加netty优化jvm参数

--- bin/set-env.sh
+++ bin/set-env.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
+
+#io.netty.leakDetectionLevel
+#netty的内存泄露检测分为四级：
+#DISABLED: 不进行内存泄露的检测；
+#SIMPLE: 抽样检测，且只对部分方法调用进行记录，消耗较小，有泄漏时可能会延迟报告，默认级别；
+#ADVANCED: 抽样检测，记录对象最近几次的调用记录，有泄漏时可能会延迟报告；
+#PARANOID: 每次创建一个对象时都进行泄露检测，且会记录对象最近的详细调用记录。是比较激进的内存泄露检测级别，消耗最大，建议只在测试时使用。
+
+#Dio.netty.noKeySetOptimization
+#是否禁用nio Selector.selectedKeys优化, 通过反射实现, 默认false
+
 JVM_FLAGS="-Dio.netty.leakDetectionLevel=advanced -Dio.netty.noKeySetOptimization=false"
-JVM_FLAGS="$JVM_FLAGS -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8008"
+
+#开启远程调试
+#JVM_FLAGS="$JVM_FLAGS -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8008"
