998d008a720adf333a8940720e21b7a9c75e447e
夜色 <yanxx@shinemo.com>
2016-10-15 13:02:40 +0800

    增加netty优化jvm参数

--- bin/set-env.sh
+++ bin/set-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#io.netty.leakDetectionLevel
+#io.netty.leakDetection.level
 #netty的内存泄露检测分为四级：
 #DISABLED: 不进行内存泄露的检测；
 #SIMPLE: 抽样检测，且只对部分方法调用进行记录，消耗较小，有泄漏时可能会延迟报告，默认级别；
@@ -10,7 +10,7 @@
 #Dio.netty.noKeySetOptimization
 #是否禁用nio Selector.selectedKeys优化, 通过反射实现, 默认false
 
-JVM_FLAGS="-Dio.netty.leakDetectionLevel=advanced -Dio.netty.noKeySetOptimization=false"
+JVM_FLAGS="-Dio.netty.leakDetection.level=advanced -Dio.netty.noKeySetOptimization=false"
 
 #开启远程调试
 #JVM_FLAGS="$JVM_FLAGS -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8008"
--- mpush-test/src/test/java/com/mpush/test/sever/ServerTestMain.java
+++ mpush-test/src/test/java/com/mpush/test/sever/ServerTestMain.java
@@ -28,6 +28,8 @@ import com.mpush.bootstrap.Main;
  */
 public class ServerTestMain {
     public static void main(String[] args) {
+        System.setProperty("io.netty.leakDetection.level", "PARANOID");
+        System.setProperty("io.netty.noKeySetOptimization", "false");
         Main.main(args);
     }
 }
