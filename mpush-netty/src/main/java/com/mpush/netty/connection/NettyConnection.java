e7e6c8ca6530f5b37870c8410d04eb71a16f78df
夜色 <yanxx@shinemo.com>
2016-12-29 12:06:08 +0800

    代码优化，防止死锁

--- mpush-netty/src/main/java/com/mpush/netty/connection/NettyConnection.java
+++ mpush-netty/src/main/java/com/mpush/netty/connection/NettyConnection.java
@@ -97,7 +97,9 @@ public final class NettyConnection implements Connection, ChannelFutureListener 
 
             //阻塞调用线程还是抛异常？
             //return channel.newPromise().setFailure(new RuntimeException("send data too busy"));
-            future.awaitUninterruptibly(100);
+            if (!future.channel().eventLoop().inEventLoop()) {
+                future.awaitUninterruptibly(100);
+            }
             return future;
         } else {
             /*if (listener != null) {
