71b76a5e93824f42f5921aa70624b6dfb3afe25d
夜色 <yanxx@shinemo.com>
2017-05-16 09:30:08 +0800

    增加注释

--- mpush-api/src/main/java/com/mpush/api/service/FutureListener.java
+++ mpush-api/src/main/java/com/mpush/api/service/FutureListener.java
@@ -5,16 +5,16 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FutureListener extends CompletableFuture<Boolean> implements Listener {
-    private final Listener l;// 防止Listener被重复执行
-    private final AtomicBoolean started;// 防止Listener被重复执行
+    private final Listener listener;
+    private final AtomicBoolean started;
 
     public FutureListener(AtomicBoolean started) {
         this.started = started;
-        this.l = null;
+        this.listener = null;
     }
 
-    public FutureListener(Listener l, AtomicBoolean started) {
-        this.l = l;
+    public FutureListener(Listener listener, AtomicBoolean started) {
+        this.listener = listener;
         this.started = started;
     }
 
@@ -22,21 +22,21 @@ public class FutureListener extends CompletableFuture<Boolean> implements Listen
     public void onSuccess(Object... args) {
         if (isDone()) return;// 防止Listener被重复执行
         complete(started.get());
-        if (l != null) l.onSuccess(args);
+        if (listener != null) listener.onSuccess(args);
     }
 
     @Override
     public void onFailure(Throwable cause) {
         if (isDone()) return;// 防止Listener被重复执行
         completeExceptionally(cause);
-        if (l != null) l.onFailure(cause);
+        if (listener != null) listener.onFailure(cause);
         throw cause instanceof ServiceException
                 ? (ServiceException) cause
                 : new ServiceException(cause);
     }
 
     public void monitor(BaseService service) {
-        if (isDone()) return;
+        if (isDone()) return;// 防止Listener被重复执行
         runAsync(() -> {
             try {
                 this.get(10, TimeUnit.SECONDS);
@@ -50,6 +50,5 @@ public class FutureListener extends CompletableFuture<Boolean> implements Listen
     public boolean cancel(boolean mayInterruptIfRunning) {
         throw new UnsupportedOperationException();
     }
-
 
 }
