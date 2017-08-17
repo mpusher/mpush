afad0bbedbecc24cc159bff3f32a8390c2b49e82
夜色 <yanxx@shinemo.com>
2017-08-17 17:11:06 +0800

    增加必要说明性注释

--- mpush-api/src/main/java/com/mpush/api/common/ServerEventListener.java
+++ mpush-api/src/main/java/com/mpush/api/common/ServerEventListener.java
@@ -29,21 +29,57 @@ import com.mpush.api.spi.Plugin;
  */
 public interface ServerEventListener extends Plugin {
 
+    /**
+     * 该事件通过guava EventBus发出，实现接口的方法必须增加
+     *
+     * @Subscribe 和 @AllowConcurrentEvents注解，
+     * 并在构造函数调用EventBus.register(this);
+     */
     default void on(ServerStartupEvent event) {
     }
 
+    /**
+     * 该事件通过guava EventBus发出，实现接口的方法必须增加
+     *
+     * @Subscribe 和 @AllowConcurrentEvents注解，
+     * 并在构造函数调用EventBus.register(this);
+     */
     default void on(ServerShutdownEvent server) {
     }
 
+    /**
+     * 该事件通过guava EventBus发出，实现接口的方法必须增加
+     *
+     * @Subscribe 和 @AllowConcurrentEvents注解，
+     * 并在构造函数调用EventBus.register(this);
+     */
     default void on(RouterChangeEvent event) {
     }
 
+    /**
+     * 该事件通过guava EventBus发出，实现接口的方法必须增加
+     *
+     * @Subscribe 和 @AllowConcurrentEvents注解，
+     * 并在构造函数调用EventBus.register(this);
+     */
     default void on(KickUserEvent event) {
     }
 
+    /**
+     * 该事件通过guava EventBus发出，实现接口的方法必须增加
+     *
+     * @Subscribe 和 @AllowConcurrentEvents注解，
+     * 并在构造函数调用EventBus.register(this);
+     */
     default void on(UserOnlineEvent event) {
     }
 
+    /**
+     * 该事件通过guava EventBus发出，实现接口的方法必须增加
+     *
+     * @Subscribe 和 @AllowConcurrentEvents注解，
+     * 并在构造函数调用EventBus.register(this);
+     */
     default void on(UserOfflineEvent event) {
     }
 }
