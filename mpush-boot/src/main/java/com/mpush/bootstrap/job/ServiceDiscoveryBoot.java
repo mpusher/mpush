9d5ad8353fe991179e136640ad740068a05dc0f5
夜色 <yanxx@shinemo.com>
2017-08-23 09:30:28 +0800

    注册与发现启动代码优化

---
+++ mpush-boot/src/main/java/com/mpush/bootstrap/job/ServiceDiscoveryBoot.java
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *   ohun@live.cn (夜色)
+ */
+
+package com.mpush.bootstrap.job;
+
+import com.mpush.api.spi.common.ServiceDiscoveryFactory;
+import com.mpush.api.spi.common.ServiceRegistryFactory;
+import com.mpush.api.srd.ServiceDiscovery;
+import com.mpush.tools.log.Logs;
+
+/**
+ * Created by yxx on 2016/5/14.
+ *
+ * @author ohun@live.cn
+ */
+public final class ServiceDiscoveryBoot extends BootJob {
+
+    @Override
+    protected void start() {
+        Logs.Console.info("init service discovery waiting for connected...");
+        ServiceDiscoveryFactory.create().syncStart();
+        startNext();
+    }
+
+    @Override
+    protected void stop() {
+        stopNext();
+        ServiceDiscoveryFactory.create().syncStop();
+        Logs.Console.info("service discovery closed...");
+    }
+}
