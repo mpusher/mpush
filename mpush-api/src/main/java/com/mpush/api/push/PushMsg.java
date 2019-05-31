32fdd27c2b5fd3f68e2f44cdefdb6aae0baf5ab3
dengly <denglylove@gmail.com>
2019-05-31 11:21:06 +0800

    更新依赖

--- mpush-api/src/main/java/com/mpush/api/push/PushMsg.java
+++ mpush-api/src/main/java/com/mpush/api/push/PushMsg.java
@@ -22,18 +22,21 @@ package com.mpush.api.push;
 
 /**
  * msgId、msgType 必填
+ *
  * msgType=1 :nofication,提醒。
  * 必填:title，content。没有title，则为应用名称。
  * 非必填。nid 通知id,主要用于聚合通知。
  * content 为push  message。附加的一些业务属性，都在里边。json格式
+ *
  * msgType=2 :非通知消息。不在通知栏展示。
  * 必填：content。
+ *
  * msgType=3 :消息+提醒
  * 作为一个push消息过去。和jpush不一样。jpush的消息和提醒是分开发送的。
  */
 public final class PushMsg {
-    private final MsgType msgType; //type
-    private String msgId; //返回使用
+    private final MsgType msgType; //type 必填
+    private String msgId; //返回使用 必填
     private String content; //content
 
     public PushMsg(MsgType msgType) {
